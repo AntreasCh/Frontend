e02dd0c4c5a0936d107ce5da43a4c82fc47f713e
AntreasCh <antreaschristodoulou11@gmail.com>
2023-04-03 02:12:48 +0100

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,43 @@
-import React from 'react';
-import ParentPage from './ParentPage';
+/**
+ * This is the homepage of the application
+using bootstrap for styling it gives you all the links to navigate through the whole app
+ * @author: Dimitriana Stylianou
+ */
+import {LinkContainer} from 'react-router-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Footer from './Footer';
+import DesktopImg from './img/manny-pantoja-jaHvDkELPKU-unsplash.jpg';
+import './Footer.css';
+function homePage() {
+ return (
 
-const HomePage = () => {
-return (
-  <ParentPage>
-<div>
-<h1>Welcome to IC3</h1>
-<h2>Purpose and Focus</h2>
-<p>The International Centre for Connected Construction (IC3) combines the power of the construction industry with the research power and expertise of universities. Currently comprising over 40 partners from the construction industry, the digital sector, academia, and regional government, it offers a joint funding model for collaborative research and innovation actively supported by regional and national agendas and global partnerships.</p>
-<h2>Vision</h2>
-<ul>
-<li>Progress towards a carbon positive sector</li>
-<li>Smarter and more effective data, digital assurance, and cyber security</li>
-<li>Improved productivity through connected and digital technologies</li>
-<li>Increased safety standards through ‘Material Passports’</li>
-<li>Future proofing and repurposing buildings and infrastructure</li>
-<li>Addressing the ‘Place’ and ‘Levelling Up’ agendas for the North-East region through new knowledge and skills leading to economic growth & social prosperity.</li>
-</ul>
-<h2>Mission</h2>
-<p>IC3 develops, exploits, and connects technological innovation to address the persistent challenges within the sector, enabling the creation and adoption of smart and sustainable whole-life strategies across all aspects of the construction industry and the entire built environment ecosystem. Through the use of evidence-led collaborative research, demonstrator projects of innovation in action, business acceleration support, education reform, and knowledge exchange IC3 addresses the global challenges across the sector, enabling more effective, safer, healthier, and more sustainable buildings and infrastructure.</p>
-<h2>Partners</h2>
-<p>IC3 is a consortium of leading organisations bringing technical innovation and digital transformation to a connected construction sector. Partner organisations include:</p>
-<ul>
-<li>Academia partners</li>
-<li>Industry partners</li>
-<li>Government partners</li>
-<li>Digital partners</li>
-</ul>
-</div>
-</ParentPage>
-);
-};
+<div className='homepage'>
 
-export default HomePage;
+ <h2>This website is ideal if you are looking for papers from specific authors</h2>
+   <p2>Specifically there are many options for papers such as: </p2>
+    <ul>
+      <li> Interactivity</li> 
+      <li> Wip</li> 
+      <li> Doctoral</li>   
+      <li> Fullpapers</li>  
+      <li> Rapid</li>  
+      <li> Competition </li> 
+    </ul>
+  
+    <img src={DesktopImg} alt="Author's table"/>
+  <p>
+  <h> Photo by</h>
+   <a href="https://unsplash.com/@mann_pantoja?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"> 
+ { } Manny Pantoja </a> 
+   on { }
+   <a href="https://unsplash.com/s/photos/laptop-on-table?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+   Unsplash </a>
+  
+  </p>
+  </div>
+ );
+}
+export default homePage;
